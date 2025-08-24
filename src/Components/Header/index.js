b77643cbdb33d04cfb4257473b1db16527a9fdb9
AshimaKamboj <ashimakamboj27@gmail.com>
2025-08-24 21:48:57 +0530

    cdd header page

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,13 @@
 const Header=()=>{
 return(
     <>
-     <h1>Header Page</h1>
+     <div className="headerWrapper">
+        <div className="top-strip bg-purple">
+            <div className="container">
+                <p className="mb-0 mt-0 text-center">welcome to our site</p>
+            </div>
+        </div>
+     </div>
     </>
 )
 }
