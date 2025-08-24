d8fbd5189ba7730371af797671ad4f528a7ec982
AshimaKamboj <ashimakamboj27@gmail.com>
2025-08-24 21:31:58 +0530

    Working on home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import Home from "./Pages/Home";
 function App() {
   return (
     <>
-    
+    <BrowserRouter>
+    <Router>
+      <Route path="/" Component={<Home/>}>
+
+      </Route>
+    </Router>
+    </BrowserRouter>
     </>
   );
 }
