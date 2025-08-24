8a62fa9205ef025f2798d5f514b1004217dbd389
AshimaKamboj <ashimakamboj27@gmail.com>
2025-08-24 21:52:08 +0530

    changed color

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,7 +2,7 @@ const Header=()=>{
 return(
     <>
      <div className="headerWrapper">
-        <div className="top-strip bg-purple">
+        <div className="top-strip bg-blue">
             <div className="container">
                 <p className="mb-0 mt-0 text-center">welcome to our site</p>
             </div>
