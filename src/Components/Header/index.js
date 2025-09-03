3cac4c70d7efe70348b9cdfb279b3196c6b2fe8e
AshimaKamboj <ashimakamboj27@gmail.com>
2025-09-03 17:54:21 +0530

    added header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,6 +5,7 @@ return(
         <div className="top-strip bg-blue">
             <div className="container">
                 <p className="mb-0 mt-0 text-center">welcome to our site</p>
+                <h1>heelo</h1>
             </div>
         </div>
      </div>
