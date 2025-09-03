1743f7d83732b2c23b73cc6314d19996c0e370a2
AshimaKamboj <ashimakamboj27@gmail.com>
2025-09-03 17:55:02 +0530

    pre version edited

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,7 +5,6 @@ return(
         <div className="top-strip bg-blue">
             <div className="container">
                 <p className="mb-0 mt-0 text-center">welcome to our site</p>
-                <h1>heelo ashima</h1>
             </div>
         </div>
      </div>
